f1d50dc6f7d49e88f2e61b79f4b36c7ae1a49cfd
ZeilerRaphael <r.zeiler@htlkrems.at>
2017-05-29 22:20:54 +0200

    Delete load.php

--- UE10/Trackster/API/load.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $sql = "SELECT * FROM projects where Pid = :Pid";
-    $toChange = $database -> prepare($sql);
-    $toChange -> bindParam(':Pid',$Pid);
-    $Pid = $_GET['change'];
-    $toChange -> execute();
-    $visualize = $toChange -> fetch();
-?>
